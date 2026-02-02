c6d8e581295e182d01342cc7c96b89cb86255d9e
eesha-patel <pateleesha2651@gmail.com>
2026-02-02 15:29:04 +0530

    Update Customers.java

--- server/src/main/java/com/edutech/progressive/entity/Customers.java
+++ server/src/main/java/com/edutech/progressive/entity/Customers.java
@@ -1,69 +1,60 @@
 package com.edutech.progressive.entity;
 
-public class Customers implements Comparable<Customers>{
-    private int customerId;
-    private String name;
-    private String email;
-    private String username;
-    private String password;
-    private String role;
-    public Customers(){}
-    public Customers(int customerId, String name, String email, String username, String password) {
-        this.customerId = customerId;
-        this.name = name;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-    }
-    public int getCustomerId() {
-        return customerId;
-    }
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
-    }
+public class Customers implements Comparable<Customers> {
+   private int customerId;
+   private String name;
+   private String email;
+   private String username;
+   private String password;
+   private String role;
+   public Customers(){
     
-    @Override
-    public int compareTo(Customers o) {
-        if(this.name == null && o.name == null){
-            return 0;
-        }
-        if(this.name==null){
-            return 1;
-        }
-        if(o.name == null){
-            return -1;
-        }
-        return this.name.compareTo(o.name);
-    }
+   }
+   
+   public Customers(int customerId, String name, String email, String username, String password) {
+    this.customerId = customerId;
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    
+}
+   public int getCustomerId() {
+    return customerId;
+   }
+   public void setCustomerId(int customerId) {
+    this.customerId = customerId;
+   }
+   public String getName() {
+    return name;
+   }
+   public void setName(String name) {
+    this.name = name;
+   }
+   public String getEmail() {
+    return email;
+   }
+   public void setEmail(String email) {
+    this.email = email;
+   }
+   public String getUsername() {
+    return username;
+   }
+   public void setUsername(String username) {
+    this.username = username;
+   }
+   public String getPassword() {
+    return password;
+   }
+   public void setPassword(String password) {
+    this.password = password;
+   }
+   
+
+   @Override
+   public int compareTo(Customers o) {
+      // TODO Auto-generated method stub
+     return this.name.compareToIgnoreCase(o.name);
+   }
 
 }
